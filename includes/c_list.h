2ca16c9a2208743fe12afea5556056c65027652f
Viveli <maceo.jalbert@epitech.eu>
2022-02-23 20:24:08 +0100

    :fix: static in header

--- includes/c_list.h
+++ includes/c_list.h
@@ -25,7 +25,6 @@ c_list_t *init_list(void);
 void free_list(c_list_t *list);
 
 // c_list_index.c
-static list_element_t *allocate_elem(void *content);
 int insert_listi(c_list_t *list, unsigned int index, void *content);
 int delete_listi(c_list_t *list, unsigned int index);
 void *get_listi(c_list_t *list, unsigned int index);
